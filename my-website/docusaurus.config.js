a892bae9247cf907923385375dd3149065efd32f
ikuokuo <ikuokuo@hotmail.com>
2020-10-07 16:10:50 +0800

    chore: docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://ikuokuo.github.io',
+  baseUrl: '/start-docusaurus2/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ikuokuo',
+  projectName: 'start-docusaurus2',
   themeConfig: {
     navbar: {
       title: 'My Site',
